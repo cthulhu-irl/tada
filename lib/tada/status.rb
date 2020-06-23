e356bd8ddbe68f058b0497c10e03c5dff28cf6fc
cthulhu-irl <xh4x0r3r@gmail.com>
2020-06-23 18:52:56 +0430

    feat: implement TADA::Status class

--- lib/tada/status.rb
+++ lib/tada/status.rb
@@ -2,20 +2,49 @@ module TADA
   class Status
     include Comparable
 
-    def initialize(stat)
-      @stat = stat
-    end
+    INDEX_STR = 0
+    INDEX_SYM = 1
 
-    def inspect()
+    CMAP = { '-' => 0, 'x' => 1, '+' => 2 }
+    SMAP = { todo: 0, doing: 1, done: 2 }
+    MAP = [
+      ['-', :todo],
+      ['x', :doing],
+      ['+', :done]
+    ]
+
+    def initialize(stat)
+      if stat.is_a? String
+        @int_stat = CMAP.fetch(stat)
+
+      elsif stat.is_a? Symbol
+        @int_stat = SMAP.fetch(stat)
+
+      elsif stat.is_a? Integer and stat >= 0
+        @int_stat = stat
+
+      else
+        raise TypeError, 'expected Integer, String or Symbol'
+      end
+
+      @sym_stat = MAP[@int_stat][INDEX_SYM]
+      @str_stat = MAP[@int_stat][INDEX_STR]
     end
 
     def to_s()
+      @str_stat
     end
 
-    def to_c()
+    def to_sym()
+      @sym_stat
+    end
+
+    def to_i()
+      @int_stat
     end
 
     def <=>(other)
+      @int_stat <=> other.to_i
     end
   end
 end
