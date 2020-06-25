926034ec47224eadb3f0d0c25ee91abd33cd9788
cthulhu-irl <xh4x0r3r@gmail.com>
2020-06-25 21:35:59 +0430

    feat(jsonparser): add guiding internal comments to all methods

--- lib/tada/parsers/json.rb
+++ lib/tada/parsers/json.rb
@@ -10,10 +10,21 @@ module TADA
       STAT_DOING = 'doing'
       STAT_DONE  = 'done'
 
+      def self.raw_dump(todo_list)
+        # convert each entry in the list to a hash object
+        # recurse on each entry by its sublist if sublist isn't empty
+        # return the result
+      end
+
       def self.load(str)
+        # load str by builtin json parser
+        # validate the result
+        # return the result if valid, otherwise raise an error
       end
 
       def self.dump(todo_list)
+        # call self.raw_dump with given list to get a raw dump
+        # convert the raw dump to json string and return
       end
     end
   end
