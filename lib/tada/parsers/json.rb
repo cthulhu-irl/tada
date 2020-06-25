ddd6b56a2e03da101f6f13824c60885787788aa8
cthulhu-irl <xh4x0r3r@gmail.com>
2020-06-25 21:05:39 +0430

    feat: add constants to TADA::Parsers::JSONParser

--- lib/tada/parsers/json.rb
+++ lib/tada/parsers/json.rb
@@ -1,6 +1,15 @@
 module TADA
   module Parsers
     class JSONParser
+      KEY_STATUS  = 'status'
+      KEY_TITLE   = 'title'
+      KEY_INFO    = 'info'
+      KEY_SUBLIST = 'sublist'
+
+      STAT_TODO  = 'todo'
+      STAT_DOING = 'doing'
+      STAT_DONE  = 'done'
+
       def self.load(str)
       end
 
