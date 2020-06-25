f7c17e5ae81dbe2ad488d590aa67440cf2b2f894
cthulhu-irl <xh4x0r3r@gmail.com>
2020-06-25 14:54:42 +0430

    feat: add guiding internal comments to TADA::Ref methods

--- lib/tada/ref.rb
+++ lib/tada/ref.rb
@@ -1,7 +1,11 @@
 module TADA
   class Ref
     def initialize(*refs)
-      # TODO
+      # flatten the refs
+      # check each ref has a valid type
+        # if ref is a Ref, replace it by its to_a
+        # if ref is a String, parse it and replace it
+      # flatten the refs and save
     end
 
     def is_singular()
