997c1117d3374309364f2be910ff59f716a454bf
cthulhu-irl <xh4x0r3r@gmail.com>
2020-06-26 13:36:14 +0430

    feat(ref): implement TADA::Ref methods (initialize)

--- lib/tada/ref.rb
+++ lib/tada/ref.rb
@@ -2,10 +2,30 @@ module TADA
   class Ref
     def initialize(*refs)
       # flatten the refs
+      refs.flatten!
+
       # check each ref has a valid type
-        # if ref is a Ref, replace it by its to_a
-        # if ref is a String, parse it and replace by its to_a
+      refs.each_with_index do |ref, i|
+        if not [Integer, Range, Hash, Ref].include? ref.class
+          raise TypeError, \
+            "each ref must be Integer, Range, Hash, or Ref"
+        end
+
+        # if hash, then make sure keys are string and values are regex
+        if ref.is_a?(Hash)
+          ref.each_pair do |k, v|
+            if not (k.is_a?(String) and v.is_a?(Regexp))
+              raise TypeError, "hash ref must be String:Regexp"
+            end
+          end
+        end
+
+        # if ref is a Ref, convert it to array
+        refs[i] = ref.to_a if ref.is_a?(Ref)
+      end
+
       # flatten the refs and save
+      @refs = refs.flatten
     end
 
     def is_singular()
