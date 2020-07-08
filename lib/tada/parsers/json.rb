84021de9852e3a70644f4babebaedb2f2ec06dc1
cthulhu-irl <xh4x0r3r@gmail.com>
2020-07-09 03:35:34 +0430

    doc: add documentation for TADA::Parsers::JSONParser methods

--- lib/tada/parsers/json.rb
+++ lib/tada/parsers/json.rb
@@ -2,6 +2,7 @@ require 'tada/todo'
 
 module TADA
   module Parsers
+    # json serializer/deserializer for todo list
     class JSONParser
       KEY_STATUS  = 'status'
       KEY_TITLE   = 'title'
@@ -15,6 +16,10 @@ module TADA
       STATMAP = [STAT_TODO, STAT_DOING, STAT_DONE]
       STATRMAP = { STAT_TODO => 0, STAT_DOING => 1, STAT_DONE => 2 }
 
+      # Convert TADA::TODO instances to a serializable form.
+      #
+      # @param [Array<TADA::TODO>] todo_list
+      # @return [Hash] json compatible representation of +todo_list+
       def self.raw_dump(todo_list)
         todo_list.map do |todo|
           # convert each entry in the list to a hash object
@@ -31,6 +36,10 @@ module TADA
         end
       end
 
+      # Convert given string to a todo list array.
+      #
+      # @param [String] str
+      # @return [Array<TADA::TODO>]
       def self.load(str)
         # load str by builtin json parser
         raw = JSON.load(str)
@@ -44,6 +53,10 @@ module TADA
         TADA::TODO.new(status, title, info: info, sublist: sublist)
       end
 
+      # Convert given +todo_list+ to a json string.
+      #
+      # @param [Array<TADA::TODO>] todo_list
+      # @return [String] json string.
       def self.dump(todo_list)
         # raw_dump, then convert to json string
         JSON.pretty_generate(JSONParser.raw_dump(todo_list))
