673a8895f162df60c579ef016704796cc59a329f
cthulhu-irl <xh4x0r3r@gmail.com>
2020-07-21 07:12:43 +0430

    test(status): add test cases for #to_s, #to_sym, #to_i, #<=> and Comparable mixin

--- spec/status_spec.rb
+++ spec/status_spec.rb
@@ -43,4 +43,29 @@ RSpec.describe Status do
       expect { Status.to_i({ todo: 2 }) }.to raise_error TypeError
     end
   end
+
+  it '#to_s' do
+    expect(Status.new(:todo).to_s).to be '-'
+    expect(Status.new(:doing).to_s).to be 'x'
+    expect(Status.new(:done).to_s).to be '+'
+  end
+
+  it '#to_sym' do
+    expect(Status.new('-').to_sym).to be :todo
+    expect(Status.new('x').to_sym).to be :doing
+    expect(Status.new('+').to_sym).to be :done
+  end
+
+  it '#to_i' do
+    expect(Status.new('-').to_i).to be 0
+    expect(Status.new('x').to_i).to be 1
+    expect(Status.new('+').to_i).to be 2
+  end
+
+  it 'comparable and spaceship <=> operator' do
+    expect(Status.new(:todo) < Status.new(:done)).to be true
+    expect(Status.new('-') >= Status.new('x')).to be false
+    expect(Status.new(1) == Status.new(1)).to be true
+    expect(Status.new(1) <=> Status.new(1)).to be 0
+  end
 end
