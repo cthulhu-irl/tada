d815bebe16e7479fef48f23e5bd415fc288db283
cthulhu-irl <xh4x0r3r@gmail.com>
2020-07-29 13:05:24 +0430

    test(jsonparser): add test skeleton for TADA::Parsers::JSONParser

---
+++ spec/json_parser_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'tada'
+
+include TADA
+include TADA::Parsers
+
+RSpec.describe JSONParser do
+  describe '::raw_load' do
+  end
+
+  describe '::raw_dump' do
+  end
+
+  describe '::load' do
+  end
+
+  describe '::dump' do
+  end
+end
